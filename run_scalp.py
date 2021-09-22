abbb802cb6e379e098f6ca6cd1338369b6f407fa
Jacob Miske <jacobmiske@gmail.com>
2021-09-21 21:32:41 -0400

    media folder and requirements txt

--- run_scalp.py
+++ run_scalp.py
@@ -76,6 +76,15 @@ class ScALP(cmd.Cmd):
               break
       else:
           break
+    capture = cv2.VideoCapture('./foreground_video.avi')
+    frameIds = capture.get(cv2.CAP_PROP_FRAME_COUNT) * np.random.uniform(size=25)
+    frames = []
+    for frame_id in frameIds:
+      capture.set(cv2.CAP_PROP_FRAME_COUNT, frame_id)
+      ret, frame = capture.read()
+      frames.append(frame)
+    median_frame = np.median(frames, axis=0).astype(dtype=np.uint8)
+    cv2.imwrite('./foreground_frame.jpg', median_frame)
     cam.release()
     result.release()
     cv2.destroyAllWindows()
@@ -97,7 +106,7 @@ class ScALP(cmd.Cmd):
       fore_gray = cv2.cvtColor(fore_frame, cv2.COLOR_BGR2GRAY)
       back_frames.append(back_gray)
       fore_frames.append(fore_gray)
-    threshold = 20
+    threshold = 40
     for i in range(0, 5):
       print("frame: " + str(i))
       f_frame = fore_frames[i]
@@ -115,7 +124,6 @@ class ScALP(cmd.Cmd):
     sizes = list(sizes)
     main_label = max(sizes)
     res_list = [i for i, value in enumerate(sizes) if value == main_label]
-    print(res_list)
     main_label = res_list[0]
     for x in range(0, 720):
       for y in range(0, 1280):
@@ -124,17 +132,62 @@ class ScALP(cmd.Cmd):
         else:
           labeled_image[x, y] = 255
     cv2.imwrite('./labels.jpg', labeled_image)
-    print(labeled_image)
     image_bw = ndimage.binary_fill_holes(labeled_image).astype(int)
     cv2.imwrite('./label_bw_filled_in.jpg', image_bw)
 
-    while True:
-      cv2.imshow('Gray image', diff_frames[-1])
-      k = cv2.waitKey(33)
-      if k==27:    # Esc key to stop
-          break
+    # while True:
+    #   cv2.imshow('Gray image', diff_frames[-1])
+    #   k = cv2.waitKey(33)
+    #   if k==27:    # Esc key to stop
+    #       break
     cv2.destroyAllWindows()
 
+  def do_xyRGB(self, arg):
+    """
+    Similar to do_xy but looks at whole RGB
+    """
+    back = cv2.VideoCapture('./background_video.avi')
+    fore = cv2.VideoCapture('./foreground_video.avi')
+    back_frames = []
+    fore_frames = []
+    diff_frames = []
+    for i in range(60):
+      _, back_frame = back.read()
+      _, fore_frame = fore.read()
+      back_frames.append(back_frame)
+      fore_frames.append(fore_frame)
+    threshold = 110
+    for i in range(0, 5):
+      print("frame: " + str(i))
+      f_frame = fore_frames[i]
+      b_frame = back_frames[i]
+      diff_frame = np.zeros(shape=(720, 1280))
+      for x in range(0, 720):
+        for y in range(0, 1280):
+          red_pixel = abs(int(f_frame[x, y][0]) - int(b_frame[x,y][0]))
+          green_pixel = abs(int(f_frame[x, y][1]) - int(b_frame[x,y][1]))
+          blue_pixel = abs(int(f_frame[x, y][2]) - int(b_frame[x,y][2]))
+          pixel_diff = red_pixel + green_pixel + blue_pixel
+          if abs(pixel_diff) > threshold:
+            diff_frame[x, y] = 255
+      diff_frames.append(diff_frame)
+    cv2.imwrite('./diff_frame.jpg', diff_frames[0])
+    labeled_image, nb_labels = ndimage.label(diff_frames[-1], structure=np.ones((3,3)))
+    sizes = ndimage.sum(diff_frames[-1], labeled_image, range(nb_labels + 1))
+    sizes = list(sizes)
+    main_label = max(sizes)
+    res_list = [i for i, value in enumerate(sizes) if value == main_label]
+    main_label = res_list[0]
+    for x in range(0, 720):
+      for y in range(0, 1280):
+        if labeled_image[x, y] != main_label:
+          labeled_image[x, y] = 0
+        else:
+          labeled_image[x, y] = 255
+    cv2.imwrite('./labels_RGB.jpg', labeled_image)
+    image_bw = ndimage.binary_fill_holes(labeled_image).astype(int)
+    cv2.imwrite('./label_bw_filled_in_RGB.jpg', image_bw)
+    cv2.destroyAllWindows()
 
   def do_drivexy(self, arg):
     '''
@@ -149,7 +202,14 @@ class ScALP(cmd.Cmd):
     """
     Drives motor to make a circle
     """
-    
+
+  def do_information(self, arg):
+    """
+    Get info on an image
+    """
+    frame = cv2.imread('./background_frame.jpg')
+    print(frame[0,0])
+    cv2.destroyAllWindows()
 
   def do_colorpick(self, arg):
     """
